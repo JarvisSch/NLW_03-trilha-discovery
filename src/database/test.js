fd41345daff48f1de93d85c44c75f208a5036a5a
JarvisSch <jean-449@hotmail.com>
2020-10-18 20:09:17 -0300

    first commit

---
+++ src/database/test.js
@@ -0,0 +1,26 @@
+const Database = require('./db');
+
+Database.then( db => {
+    
+    db.run(`
+        INSERT INTO orphanages (
+            lat,
+            lng,
+            name,
+            whatsapp,
+            images,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        ) VALUES (
+            "-27.222633",
+            "-49.6455874",
+            "Lar das meninas",
+            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+            "https://images.unsplash.com/photo-1595009545055-d5ec0bb8d732?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+            "Horário de visitas Das 8h as 18h",
+            "1"
+        );
+    `)
+})
